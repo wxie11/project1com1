3733609c129f1cd447ba7d61fa51211368d05607
wxie <wxie11@hawk.iit.edu>
2019-02-22 15:20:50 -0600

    add simple validation

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,3 +1,8 @@
 class Menu < ApplicationRecord
   belongs_to :restaurant
+  validates :name, :image_url, :price, :restaurant, presence: true
+  validates :name, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(jpg|png)\Z}i,
+    message: 'Image must be in either JPG or PNG format.'}
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,8 @@
 class Restaurant < ApplicationRecord
   has_many :menus
+  validates :name, :phone, :address, presence: true
+  validates :name, :phone, :address, uniqueness: true
+  validates :phone, allow_blank :true, format: {
+    with: /\A\(?\d{3}\)?[- ]?\d{3}[- ]?\d{4}\z/,
+    message: 'Phone number must be in this format: 000-000-0000.'}
 end
