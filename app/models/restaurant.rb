2a5fb91a53786b4d42afba222055b6cae3a9c0f5
wxie <wxie11@hawk.iit.edu>
2019-02-22 15:43:02 -0600

    add seed data

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,7 +2,7 @@ class Restaurant < ApplicationRecord
   has_many :menus
   validates :name, :phone, :address, presence: true
   validates :name, :phone, :address, uniqueness: true
-  validates :phone, allow_blank :true, format: {
-    with: /\A\(?\d{3}\)?[- ]?\d{3}[- ]?\d{4}\z/,
-    message: 'Phone number must be in this format: 000-000-0000.'}
+  validates :phone, allow_blank: true, format: {
+    with: /[0-9]{3}-[0-9]{3}-[0-9]{4}/,
+    message: 'Phone numbers must be in xxx-xxx-xxxx format.'}
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Take the database tables back to zero record
+Menu.destroy_all
+Restaurant.destroy_all
+
+@noodle_house = Restaurant.create(
+  name: 'Noodle House',
+  address: '754 S State St',
+  phone: '312-752-7854'
+)
+
+@ajida = Restaurant.create(
+  name: 'Ajida',
+  address: '201 N Wells St',
+  phone: '312-332-6878'
+)
+
+Menu.create(
+  [
+    {name: 'Vegetarian Pasta', image_url: 'veg_pasta.jpg', price: 9.99, restaurant: @noodle_house},
+    {name: 'Miso Ramen', image_url: 'miso_ramen.jpg', price: 13.50, restaurant: @ajida},
+    {name: 'Japanese Curry Rice', image_url: 'curry_rice.jpg', price: 11.00, restaurant: @ajida}
+  ]
+)
