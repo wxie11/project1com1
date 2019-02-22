f731426f0ef28a306ed7f58fdc68430087380f70
wxie <wxie11@hawk.iit.edu>
2019-02-22 14:30:36 -0600

    run restaurant model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_22_202251) do
+ActiveRecord::Schema.define(version: 2019_02_22_202820) do
 
   create_table "menus", force: :cascade do |t|
     t.string "name"
     t.string "image_url"
     t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
